8e08a45e9e0a7fffd5cb65ac4ff9921da086fe30
Kyle Robinson <78045799+kyler0b@users.noreply.github.com>
2024-02-22 22:07:51 -0500

    api update

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,5 +1,5 @@
 const apiKey = '5e54e6587749883547fa0f97121a090c';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={5e54e6587749883547fa0f97121a090c}';
 
 const locationInput = document.getElementById('locationInput');
 const searchButton = document.getElementById('searchButton');
